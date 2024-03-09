31dfd2856914c2d9a58595627c71983baa9b4099
ltpt <lethiphuongthaokt22@gmail.com>
2024-03-10 01:25:04 +0700

    Init Spring Boot project

---
+++ next-gen-ticketing/src/main/java/com/poly/NextGenTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGenTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextGenTicketingApplication.class, args);
+	}
+
+}
