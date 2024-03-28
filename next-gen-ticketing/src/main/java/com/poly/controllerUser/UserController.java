fffc2b7ec0e300a50b4e74a546a0a84662146f28
ductai77 <ductaitruongcr7@gmail.com>
2024-03-28 11:06:54 +0700

    ductai fix something

--- next-gen-ticketing/src/main/java/com/poly/controllerUser/UserController.java
+++ next-gen-ticketing/src/main/java/com/poly/controllerUser/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
