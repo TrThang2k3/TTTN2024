e26eef831362428dc9ac2187ebcf4337d14788e7
ductai77 <ductaitruongcr7@gmail.com>
2024-04-10 01:19:45 +0700

    publisher vs ticket

--- next-gen-ticketing/src/main/java/com/poly/controllerUser/PublisherController.java
+++ next-gen-ticketing/src/main/java/com/poly/controllerUser/PublisherController.java
@@ -10,14 +10,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.poly.entity.Account;
+import com.poly.entity.Publisher;
 import com.poly.entity.Ticket;
 import com.poly.entity.Type;
 import com.poly.repository.TicketDAO;
+import com.poly.service.PublisherService;
 import com.poly.service.TicketService;
 import com.poly.util.service.SessionService;
 import com.poly.util.service.UploadService;
@@ -35,6 +38,9 @@ public class PublisherController {
     
     @Autowired
     SessionService sessionService;
+
+	@Autowired
+	PublisherService publisherService;
     
     @GetMapping("/nextgen.com/publisher")
     public String show(Model model) {
@@ -43,6 +49,8 @@ public class PublisherController {
     	Ticket ticket = new Ticket();
     	ticket.setPublisher(account.getPublisher());
     	model.addAttribute("ticket", ticket);
+		model.addAttribute("tickets", ticService.findAll());
+		model.addAttribute("publisher", account.getPublisher());
     	return "/template-user/publisher";
     }
     
@@ -65,13 +73,22 @@ public class PublisherController {
 	public List<Type> getTypes() {
 	    return ticService.findAllTypes();
 	}
-	@GetMapping("/nextgen.com/inforPublisher")
-	public String infor(Model model) {
-		Account account=getLogAcc();
-		model.addAttribute("publisher", account.getPublisher());
-
-		return "/template-user/inforPublisher";
+	@PostMapping("/nextgen.com/update/{id}")
+	public String updatePublisher(@PathVariable("id") Integer id, @ModelAttribute Publisher updatePublisher) {
+		updatePublisher.setId(id);
+		publisherService.update(updatePublisher);
+		return"redirect:/nextgen.com/publisher";
 	}
+	
+	// @GetMapping("/nextgen.com/publisher")
+	// public String test(Model model) {
+	// 	Account account=getLogAcc();
+	// 	model.addAttribute("tickets", ticService.findAll());
+	// 	model.addAttribute("publisher", account.getPublisher());
+	// 	return "/template-user/publisher";
+	// }
+	
+	
 	
    
 }
--- next-gen-ticketing/src/main/resources/Static/js/admin.js
+++ next-gen-ticketing/src/main/resources/Static/js/admin.js
@@ -1,29 +1,45 @@
-		function showAccount() {
-			document.getElementById("accountContent").classList
-					.remove("hidden");
-			document.getElementById("ticketContent").classList.add("hidden");
-		}
+function showAccount() {
+	document.getElementById("accountContent").classList
+		.remove("hidden");
+	document.getElementById("ticketContent").classList.add("hidden");
+}
 
-		function showTicket() {
-			document.getElementById("accountContent").classList.add("hidden");
-			document.getElementById("ticketContent").classList.remove("hidden");
-		}
+function showTicket() {
+	document.getElementById("accountContent").classList.add("hidden");
+	document.getElementById("ticketContent").classList.remove("hidden");
+}
 
-		function showPublisher() {
-			document.getElementById("accountContent").classList.add("hidden");
-			document.getElementById("ticketContent").classList.add("hidden");
-			document.getElementById("publisherContent").classList
-					.remove("hidden");
-		}
+function showPublisher() {
+	document.getElementById("accountContent").classList.add("hidden");
+	document.getElementById("ticketContent").classList.add("hidden");
+	document.getElementById("publisherContent").classList
+		.remove("hidden");
+}
 
-		function showInvoice() {
-			document.getElementById("accountContent").classList.add("hidden");
-			document.getElementById("ticketContent").classList.add("hidden");
-			document.getElementById("publisherContent").classList.add("hidden");
-			document.getElementById("invoiceContent").classList
-					.remove("hidden");
-		}
-		
+function showInvoice() {
+	document.getElementById("accountContent").classList.add("hidden");
+	document.getElementById("ticketContent").classList.add("hidden");
+	document.getElementById("publisherContent").classList.add("hidden");
+	document.getElementById("invoiceContent").classList
+		.remove("hidden");
+}
+
+let slideIndex = 0;
+
+function moveSlide(n) {
+	const slides = document.querySelector('.slides');
+	const slideWidth = document.querySelector('.card').offsetWidth;
+	slideIndex += n;
+	if (slideIndex >= slides.children.length) {
+		slideIndex = 0;
+	}
+	if (slideIndex < 0) {
+		slideIndex = slides.children.length - 1;
+	}
+	const moveDistance = -slideWidth * slideIndex;
+	slides.style.transform = `translateX(${moveDistance}px)`;
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////		
 
 
